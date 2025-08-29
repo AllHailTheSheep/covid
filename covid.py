4bec0da4445d354178e2254be217ef3edcdd853e
Back-Gammon <129220958+Back-Gammon@users.noreply.github.com>
2025-08-29 11:40:03 -0400

    working...

--- covid.py
+++ covid.py
@@ -5,12 +5,21 @@
 
 import math
 
-
 def infections_at_x_days(infections, rate, x):
     for i in range (0, x):
-        infections = infections * rate
-        print(i, infections)
+        math.floor(infections) = infections * rate
+
+        #print(i, infections)
     return infections
 
+def money_lost(n):
+    return n*9972
+
+def percentage(n):
+    return n/39740
+
 students_at_14_days = infections_at_x_days(7, 1.2, 14)
-print(math.floor(students_at_14_days) * 9972)
+#print(math.floor(students_at_14_days) * 9972)
+print("money lost: " + money_lost(students_at_14_days))
+print("percentage: {0:2f}".format(percentage(students_at_14_days)))
+
